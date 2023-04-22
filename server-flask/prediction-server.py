c00f25ceccc7bfb82101e10e186a2af9b354261f
Mohammad Ahmad <mahmad97taha@gmail.com>
2023-04-22 18:29:25 -0400

    add decryption to flask app

--- server-flask/prediction-server.py
+++ server-flask/prediction-server.py
@@ -1,9 +1,10 @@
-import base64
 import io
-import numpy as np
+import numpy
 
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
+from base64 import b64decode
+from Crypto.Cipher import AES
 from PIL import Image
 from tensorflow.keras.preprocessing.image import img_to_array
 
@@ -11,25 +12,45 @@ app = Flask(__name__)
 model = load_model('../model')
 classes = ['Carbuncles and Furuncles', 'Cellulitis', 'Folliculitis', 'Impetigo']
 
-@app.route('/test', methods=['GET'])
+@app.route('/', methods=['GET'])
 def test():
   return 'Flask server running'
 
 @app.route('/classify_image', methods=['POST'])
 def classify_image():
   # Get image from request
-  image = request.json['image']
-  image_binary = base64.b64decode(image)
-  buffer = io.BytesIO(image_binary)
-  img = Image.open(buffer)
-  img = img.resize((256, 256))
+  encrypted_image = request.json['image']
 
-  x = img_to_array(img)
-  x = np.expand_dims(x, axis=0)
-  result = model.predict(x)
-  prediction = classes[np.argmax(result)]
+  decrypted_image = decrypt_image(encrypted_image)
+
+  predicted_class = predict_class(decrypted_image)
   
-  return jsonify({'message': 'Image recieved', 'class': prediction})
+  return jsonify({'message': 'Image recieved', 'class': predicted_class})
+
+def decrypt_image(image):
+  ciphertext = b64decode(image)
+
+  key = b'0123456789abcdef'
+  iv = b'fedcba9876543210'
+  cipher = AES.new(key, AES.MODE_CBC, iv)
+
+  plaintext = cipher.decrypt(ciphertext)
+  decrypted_image = plaintext.decode()
+
+  return decrypted_image
+
+def predict_class(image_base64):
+  image_binary = b64decode(image_base64)
+  buffer = io.BytesIO(image_binary)
+  image = Image.open(buffer)
+  image = image.resize((256, 256))
+
+  x = img_to_array(image)
+  x = numpy.expand_dims(x, axis=0)
+  result = model.predict(x)
+  prediction = classes[numpy.argmax(result)]
+
+  return prediction
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=3001)
